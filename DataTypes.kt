1b21bcd8503f4bec90473e946f91301cd0a9179d
NarasimhaVarman <dnvtechvlogs@gmail.com>
2024-07-29 19:09:25 +0530

    DatatypesAndVariables

---
+++ DataTypes.kt
@@ -0,0 +1,27 @@
+fun main(){
+       var minnumber : Int = Int.MIN_VALUE //32 Bits (4 bytes)
+       var maxnumber : Int = Int.MAX_VALUE 
+       var minbyte : Byte = Byte.MIN_VALUE //8 bits (1 byte)
+       var maxbyte : Byte = Byte.MAX_VALUE
+       var minshort : Short = Short.MIN_VALUE //16 bits (2 bytes)
+       var maxshort : Short = Short.MAX_VALUE
+       var minlong : Long = Long.MIN_VALUE //64 bits (8 bytes)
+       var maxlong : Long = Long.MAX_VALUE
+       var minfloat = Float.MIN_VALUE//32 bits (4 Bytes)
+       var maxfloat = Float.MAX_VALUE
+       var mindouble = Double.MIN_VALUE//64 bits (8 bytes)
+       var maxdouble = Double.MAX_VALUE
+       println("minshort :$minshort")
+       println("maxshort :$maxshort")
+       println("minbyte :$minbyte")
+       println("maxbyte :$maxbyte")
+       println("minnumber: $minnumber")
+       println("maxnumber :$maxnumber")
+       println("minlong :$minlong")
+       println("maxlong :$maxlong")
+       println("minfloat :$minfloat")
+       println("maxfloat :$maxfloat")
+       println("mindouble :$mindouble")
+       println("maxdouble :$maxdouble")
+
+}
---
+++ Variables.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var username: String = "Narasimha Varma"
+    var roll: Byte = 1
+    var salary: Int = 55000
+    var score: Short = 30000
+    var phone: Long = 8639140291
+    val aadhar: Long = 440723489666
+    val age: Byte = 20 // val = read only variable
+    println("UserName: $username")
+    println("salary: $salary")
+    println("roll: $roll")
+    println("score: $score")
+    println("phone: $phone")
+    println("Aadhar: $aadhar")
+    println("Age: $age")
+}
+
